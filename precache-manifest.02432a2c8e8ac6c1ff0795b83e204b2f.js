c4e88da1f6f7ed1cb4a3d41e9598a0c8088a144f
maker <codyoferrall@gmail.com>
2020-09-20 13:49:42 -0700

    Updates

---
+++ precache-manifest.02432a2c8e8ac6c1ff0795b83e204b2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e71f5c32eff386aede24b19c43859e",
+    "url": "/tutor_site/index.html"
+  },
+  {
+    "revision": "bf8cfb3db30a84beee09",
+    "url": "/tutor_site/static/css/main.34f86871.chunk.css"
+  },
+  {
+    "revision": "a2fc870c6f8371f9e232",
+    "url": "/tutor_site/static/js/2.62f6ce01.chunk.js"
+  },
+  {
+    "revision": "00ab883b2e1923f3ad803612c1d50018",
+    "url": "/tutor_site/static/js/2.62f6ce01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf8cfb3db30a84beee09",
+    "url": "/tutor_site/static/js/main.fce9519a.chunk.js"
+  },
+  {
+    "revision": "3e96aae14903cdbdebfc",
+    "url": "/tutor_site/static/js/runtime-main.d6b5750a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tutor_site/static/media/logo.5d5d9eef.svg"
+  }
+]);
